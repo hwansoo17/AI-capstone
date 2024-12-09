45d5a4d18ae58516078c89863810d0a4d2f54a23
suhwan kim <playground99@naver.com>
2024-12-09 20:41:34 +0900

    feat: 404페이지 구현 완료

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,13 +1,28 @@
 import Link from 'next/link'
- 
+import waitingIcon from '../../public/icons/waiting.svg'
+import Image from 'next/image'
 export default function NotFound() {
   return (
-    <div className="w-full bg-white items-center px-5">
-      <div className="max-w-[1280px] min-w-[1000px] mx-auto flex flex-col items-center gap-4 px-32 py-14 justify-between">
-        <h2>준비중 입니다</h2>
-        <Link href="/" className='text-black'>
+    <div className="w-full bg-white items-center">
+      <div className="max-w-[1280px] min-w-[1000px] mx-auto flex flex-col items-center gap-4 px-32 py-12 justify-between">
+        <Image src={waitingIcon} alt="waiting" className='mt-2'/>
+        <h1 className="font-bold text-3xl text-gray-800 mt-8">
+        현재 페이지 준비중이에요!
+        </h1>
+        <p className="font-medium text-xl text-[#949494] mt-3 text-center">
+        이용에 불편을 드려 죄송합니다<br/>더 나은 서비스를 제공하기 위해 노력하겠습니다
+        </p>
+        {/* <Link href="/" className='text-black'>
           홈으로 돌아가기
-        </Link>
+        </Link> */}
+        <div className="flex justify-center mt-20">
+          <Link 
+            href="/"
+            className="text-2xl bg-[#0F2F70] text-white font-semibold rounded-full px-12 py-6 hover:bg-[#0D265E]"
+          >
+            홈으로 돌아가기
+          </Link>
+        </div>
       </div>
     </div>
   )
